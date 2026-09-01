c5ca5bff53ef824a1e1959c80e7b204691210ab1
Rocco Antoniou <roccoantoniou@gmail.com>
2026-08-31 23:40:18 -0600

    Use system clipboard in Neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -33,6 +33,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
 vim.opt.smartcase = false
 
